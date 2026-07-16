bcd73d6288aeb4e2d70eadbb206d21754cf4dfd3
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-07-16 05:09:10 -0700

    the style has been upgraded

--- frontend/scanner.js
+++ frontend/scanner.js
@@ -1,35 +1,114 @@
 function onScanSuccess(decodedText) {
 
+    let action = "";
+    let value = decodedText;
+
+    if (decodedText.startsWith("http://") || decodedText.startsWith("https://")) {
+
+        action = `
+            <a href="${decodedText}" target="_blank" class="action-btn">
+                🌐 Open Website
+            </a>
+        `;
+
+    }
+
+    else if (decodedText.startsWith("mailto:")) {
+
+        const email = decodedText.replace("mailto:", "");
+
+        action = `
+            <a href="mailto:${email}" class="action-btn">
+                📧 Send Email
+            </a>
+        `;
+
+        value = email;
+
+    }
+
+    else if (decodedText.startsWith("tel:")) {
+
+        const phone = decodedText.replace("tel:", "");
+
+        action = `
+            <a href="tel:${phone}" class="action-btn">
+                📞 Call Number
+            </a>
+        `;
+
+        value = phone;
+
+    }
+
+    else if (decodedText.startsWith("sms:")) {
+
+        const sms = decodedText.replace("sms:", "");
+
+        action = `
+            <a href="sms:${sms}" class="action-btn">
+                💬 Send SMS
+            </a>
+        `;
+
+        value = sms;
+
+    }
+
+    else if (decodedText.startsWith("WIFI:")) {
+
+        action = `
+            <button class="action-btn">
+                📶 Wi-Fi QR Detected
+            </button>
+        `;
+
+    }
+
+    else {
+
+        action = `
+            <button onclick="copyText()" class="action-btn">
+                📋 Copy Text
+            </button>
+        `;
+
+    }
+
     document.getElementById("scanResult").innerHTML = `
 
-        <h3>✅ Scan Successful</h3>
+        <div class="scan-card">
 
-        <p>${decodedText}</p>
+            <h2>✅ Scan Successful</h2>
 
-        <br>
+            <p id="qrText">${value}</p>
 
-        <a href="${decodedText}" target="_blank">
+            ${action}
 
-            Open Link
-
-        </a>
+        </div>
 
     `;
+
+    scanner.clear();
+
+}
+
+function copyText(){
+
+    const text = document.getElementById("qrText").innerText;
+
+    navigator.clipboard.writeText(text);
+
+    alert("Copied Successfully!");
 
 }
 
 const scanner = new Html5QrcodeScanner(
-
     "reader",
-
     {
-
         fps:10,
-
         qrbox:250
-
     }
-
 );
 
 scanner.render(onScanSuccess);
